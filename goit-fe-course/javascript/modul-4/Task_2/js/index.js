b182bed005bc898acc73cb77d96064f5a48ef752
Kalashnyk Bohdan <space.bohdan@gmail.com>
2019-12-05 00:16:05 +0200

    new

--- goit-fe-course/javascript/modul-4/Task_2/js/index.js
+++ goit-fe-course/javascript/modul-4/Task_2/js/index.js
@@ -5,8 +5,46 @@
 
 // Task 2
 
+// const inventory = {
+//   items: ['Монорельса', 'Фильтр'],
+//   add(itemName) {
+//     console.log(`Adding ${itemName} to inventory`);
+
+//     this.items.push(itemName);
+//   },
+//   remove(itemName) {
+//     console.log(`Removing ${itemName} from inventory`);
+
+//     inventory.items = inventory.items.filter(item => item !== itemName);
+//   },
+// };
+
+// const invokeInventoryAction = function(itemName, action) {
+//   console.log(`Invoking action on ${itemName}`);
+//   switch(action){
+//     case 'add':
+//         inventory.add(itemName);
+//         break;
+//     case 'remove':
+//         inventory.remove(itemName);
+//         break;
+//     default: 'Нет такого действия'
+//   }
+// };
+
+// invokeInventoryAction('Аптечка', 'add');
+
+
+// console.log(inventory.items);
+
+// invokeInventoryAction('Фильтр', 'remove');
+
+
+// console.log(inventory.items);
+
+// ==============================
 const inventory = {
-  items: ['Монорельса', 'Фильтр'],
+  items: ['Knife', 'Gas mask'],
   add(itemName) {
     console.log(`Adding ${itemName} to inventory`);
 
@@ -15,29 +53,26 @@ const inventory = {
   remove(itemName) {
     console.log(`Removing ${itemName} from inventory`);
 
-    inventory.items = inventory.items.filter(item => item !== itemName);
+    this.items = this.items.filter(item => item !== itemName);
   },
 };
 
 const invokeInventoryAction = function(itemName, action) {
   console.log(`Invoking action on ${itemName}`);
-  switch(action){
-    case 'add':
-        inventory.add(itemName);
-        break;
-    case 'remove':
-        inventory.remove(itemName);
-        break;
-    default: 'Нет такого действия'
-  }
+  action(itemName);
 };
 
-invokeInventoryAction('Аптечка', 'add');
+const pushInventoryAdd = inventory.add.bind(inventory);
+const pushInventoryRemove = inventory.remove.bind(inventory);
 
+invokeInventoryAction('Medkit', pushInventoryAdd);
+// Invoking action on Medkit
+// Adding Medkit to inventory
 
-console.log(inventory.items);
+console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
 
-invokeInventoryAction('Фильтр', 'remove');
+invokeInventoryAction('Gas mask', pushInventoryRemove);
+// Invoking action on Gas mask
+// Removing Gas mask from inventory
 
-
-console.log(inventory.items);
+console.log(inventory.items); // ['Knife', 'Medkit']
